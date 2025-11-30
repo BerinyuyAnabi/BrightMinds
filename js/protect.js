56137de8e906f506c1e538ab9ff7b421142cbb7f
Vanessa Logan Berinyuy Anabi <vanessalogan@Vanessas-MacBook-Air.local>
2025-11-30 18:13:21 +0000

    auth changes

--- js/protect.js
+++ js/protect.js
@@ -2,7 +2,7 @@
 (function() {
     const session = localStorage.getItem('brightMindsSession');
     if (!session) {
-        window.location.replace('/../index.html');
-        // window.location.href = '/index.html';
+        // window.location.replace('/../index.html');
+        window.location.href = 'http://169.239.251.102:341/~logan.anabi/BrightMinds/BrightMinds/index.html';
     }
 })();
