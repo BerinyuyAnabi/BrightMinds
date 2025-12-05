1670849b2ab7fa97d1c16570366e1cbb68c71ae7
Vanessa Logan Berinyuy Anabi <vanessalogan@Vanessas-MacBook-Air.local>
2025-12-05 01:59:17 +0000

    updated files

--- js/protect.js
+++
@@ -1,8 +0,0 @@
-// Simple authentication check for protected pages
-(function() {
-    const session = localStorage.getItem('brightMindsSession');
-    if (!session) {
-        // window.location.replace('../index.html');
-        window.location.href = '../index.html';
-    }
-})();
